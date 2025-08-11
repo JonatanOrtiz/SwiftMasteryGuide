f7c86a58bc907bca824caa164151a96752007477
JonatanOrtiz <jonataneduard@gmail.com>
2025-08-11 19:22:14 -0300

    Initial Commit

---
+++ SwiftMasteryGuide/SwiftMasteryGuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftMasteryGuideApp.swift
+//  SwiftMasteryGuide
+//
+//  Created by Jonatan Ortiz on 11/08/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftMasteryGuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
