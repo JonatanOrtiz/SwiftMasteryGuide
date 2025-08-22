e66ba81d4124ea60d41479089d89336836860b7c
JonatanOrtiz <jonataneduard@gmail.com>
2025-08-22 01:58:45 -0300

    Creates Push Notification

--- SwiftMasteryGuide/SwiftMasteryGuideApp.swift
+++ SwiftMasteryGuide/SwiftMasteryGuideApp.swift
@@ -9,8 +9,6 @@ import SwiftUI
 
 @main
 struct SwiftMasteryGuideApp: App {
-    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
-
     var body: some Scene {
         WindowGroup {
             TechnologiesListView()
